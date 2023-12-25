3522f2fa0a189872b7ec525db7f4efd8cb1fdde7
Viorel <viorel.stoica@student.tuiasi.ro>
2023-12-25 15:40:22 +0200

    butoane, restart de video, curatenie

--- main.py
+++ main.py
@@ -53,15 +53,16 @@ def calculPunctMijloc(lista_puncte):
     return mijlocx, mijlocy
 
 
-def filtruDelimitare(imagine_delimitata, lista_puncte, latime, inaltime):
+def filtruDelimitare(lista_puncte, latime, inaltime):
     mascaCreata = np.zeros((int(latime)+2, int(inaltime)+2), np.uint8)
+    imagine_delimitata = np.zeros((int(latime), int(inaltime)), np.uint8)
     if len(lista_puncte) >= 2:  # desenam liniile initiale
         for index_puncte in range(-1, len(lista_puncte) - 1, 1):
-            cv2.line(imagine_delimitata, lista_puncte[index_puncte], lista_puncte[index_puncte + 1], (255, 0, 0), 1)
+            cv2.line(imagine_delimitata, lista_puncte[index_puncte], lista_puncte[index_puncte + 1], (255, 255, 255), 1)
         if len(lista_puncte) >= 3:  # facem fill
             punctMijloc = calculPunctMijloc(lista_puncte)
-            print(punctMijloc)
-            #cv2.circle(imagine_delimitata, (int(punctMijloc[0]), int(punctMijloc[1])), 10, (0, 0, 255), 3)
+            # print(punctMijloc)
+            # cv2.circle(imagine_delimitata, (int(punctMijloc[0]), int(punctMijloc[1])), 10, (0, 0, 255), 3)
             x_maxim_stanga, x_maxim_dreapta = punctMijloc[0], punctMijloc[1]
             for index_puncte in range(-1, len(lista_puncte)-1, 1):
                 if (lista_puncte[index_puncte][1] <= punctMijloc[1] <= lista_puncte[index_puncte + 1][1]) or (
@@ -71,20 +72,27 @@ def filtruDelimitare(imagine_delimitata, lista_puncte, latime, inaltime):
                                     (lista_puncte[index_puncte+1][0]-lista_puncte[index_puncte][0])) / \
                                    (lista_puncte[index_puncte+1][1]-lista_puncte[index_puncte][1]) + \
                                    lista_puncte[index_puncte][0]
-                    #cv2.circle(imagine_delimitata, (int(coordonata_x), int(punctMijloc[1])), 10, (0, 255, 0), 3)
+                    # cv2.circle(imagine_delimitata, (int(coordonata_x), int(punctMijloc[1])), 10, (0, 255, 0), 3)
                     if coordonata_x < x_maxim_stanga:
                         x_maxim_stanga = coordonata_x
                     elif coordonata_x > x_maxim_dreapta:
                         x_maxim_dreapta = coordonata_x
-            #cv2.circle(imagine_delimitata, (int(x_maxim_stanga), int(punctMijloc[1])), 10, (255, 0, 0), 3)
-            #cv2.circle(imagine_delimitata, (int(x_maxim_dreapta), int(punctMijloc[1])), 10, (255, 0, 0), 3)
+            # cv2.circle(imagine_delimitata, (int(x_maxim_stanga), int(punctMijloc[1])), 10, (255, 0, 0), 3)
+            # cv2.circle(imagine_delimitata, (int(x_maxim_dreapta), int(punctMijloc[1])), 10, (255, 0, 0), 3)
             if x_maxim_stanga != punctMijloc[0]:
-                cv2.floodFill(imagine_delimitata, mascaCreata, (int(x_maxim_stanga)+5, int(punctMijloc[1])), (255, 0, 0))
+                cv2.floodFill(imagine_delimitata, mascaCreata, (int(x_maxim_stanga)+5, int(punctMijloc[1])), (255, 255, 255))
             else:
-                cv2.floodFill(imagine_delimitata, mascaCreata, (int(x_maxim_dreapta)-5, int(punctMijloc[1])), (255, 0, 0))
-    else:
-        print("Nu am destule puncte!" + str(len(lista_puncte)))
+                cv2.floodFill(imagine_delimitata, mascaCreata, (int(x_maxim_dreapta)-5, int(punctMijloc[1])), (255, 255, 255))
+    # else:
+        # print("Nu am destule puncte!" + str(len(lista_puncte)))
     return imagine_delimitata
+
+
+def regiuniDelimitare(imagine, lista_puncte):
+    if len(lista_puncte) >= 2:  # desenam liniile initiale
+        for index_puncte in range(-1, len(lista_puncte) - 1, 1):
+            cv2.line(imagine, lista_puncte[index_puncte], lista_puncte[index_puncte + 1], (255, 0, 0), 3)
+    return imagine
 
 
 listaPuncte = list()
@@ -100,28 +108,35 @@ def callbackMouse(event, mausx, mausy, flags, param):
         listaPuncte.clear()
 
 
+def callbackButonIncarcareMasca(par1, par2):
+    print(par1, par2)
+
+
+def callbackButonSalvareMasca(par1, par2):
+    print(par1, par2)
+
+
+def callbackButonUrmatoareaMasca(par1, par2):
+    print(par1, par2)
+
+
 if __name__ == '__main__':
     frame_buffer = deque(maxlen=100)
     mask_buffer = deque(maxlen=100)
     current_frame_index = 0
-    nume_imagine_filtru = 'COX'
+    nume_imagine_filtru = 'Desenare_Filtru'
     pause = False
     cap = cv2.VideoCapture('intersectie.mp4')
     cv2.namedWindow(nume_imagine_filtru)
     cv2.setMouseCallback(nume_imagine_filtru, callbackMouse)
-
+    cv2.createButton("Incarcare masca", callbackButonIncarcareMasca, cv2.QT_PUSH_BUTTON)
+    cv2.createButton("Salvare masca", callbackButonSalvareMasca, cv2.QT_PUSH_BUTTON)
+    cv2.createButton("Urmatoarea masca", callbackButonUrmatoareaMasca, cv2.QT_PUSH_BUTTON)
     events = [i for i in dir(cv2) if 'EVENT' in i]  # printeaza toate evenimentele disponibile
     print(events)
 
     MINAREA = 300
     MAXAREA = 50000
-    framenumber = 0  # keeps track of current frame
-    LLcars = 0  # masini de pe partea stanga (LL = Left Lane)
-    MLcars = 0  # masini de pe partea de mijloc
-    RLcars = 0  # masini de pe partea dreapta
-    carids = []  # id-ul masinilor
-    caridcrossed = []  # id-ul masinilor ce au trecut de bariera
-    totalcars = 0  # toate masinile ce au trecut de bariera
 
     # o solutie foarte buna de a detecta obiecte ce se misca pe un background static
     # folosind backgroundSubtractor
@@ -132,9 +147,19 @@ if __name__ == '__main__':
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
     kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
 
+    read_frame_index = 0
     while True:
         if not pause:
             ret, frame = cap.read()
+            if frame is None and read_frame_index == 0:  # nu a citit frame, probabil path prost de fisier
+                print("Nu am putut citi niciun frame!")
+                break
+            elif frame is None and read_frame_index != 0:  # a ajuns la sfarsit, reseteaza frame-ul
+                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                read_frame_index = 0
+                ret, frame = cap.read()
+            else:
+                read_frame_index += 1
             frame = cv2.resize(frame, (0, 0), None, ratio, ratio)  # resize image
             resizedWidth, resizedHeight, channels = frame.shape
             image = frame.copy()
@@ -200,18 +225,18 @@ if __name__ == '__main__':
         elif key == ord('d'):  # Press '>' to move forward
             if pause and current_frame_index + 1 < len(frame_buffer) - 1:
                 current_frame_index += 1
-
         # aplicare filtru delimitare pe poza originala
-        listaPuncte1 = ((100, 100), (100, 200), (200, 200), (200, 100), (300, 150))
-        sursa_temporara_filtru = np.zeros((resizedWidth, resizedHeight, 3), np.uint8)
-        imagineDelimitata = filtruDelimitare(sursa_temporara_filtru, listaPuncte, resizedWidth, resizedHeight)
-        cv2.imshow("COX", imagineDelimitata)
+        #sursa_temporara_filtru = np.zeros((resizedWidth, resizedHeight, 3), np.uint8)
+        imagineDelimitata = filtruDelimitare(listaPuncte, resizedWidth, resizedHeight)
+        imagineRegionata = regiuniDelimitare(frame, listaPuncte)
+        cv2.imshow(nume_imagine_filtru, imagineRegionata)
+
         # inceput afisare
-        imagini = [frame, mask_buffer[-1], frame_buffer[-1], imagineDelimitata]  # lista cu imagini de afisat
+        imagini = [frame, mask_buffer[-1], frame_buffer[-1], imagineDelimitata, imagineRegionata]  # lista cu imagini de afisat
         imagini2 = [frame, mask_buffer[current_frame_index], frame_buffer[current_frame_index],
-                    imagineDelimitata]  # lista cu imagini de afisat
-        texte = ["original", "finalMask", "image", "imagine delimitata"]  # lista cu numele fiecarei imagini
-        numar_de_imagini_pe_linie = 2
+                    imagineDelimitata, imagineRegionata]  # lista cu imagini de afisat
+        texte = ["frame "+str(read_frame_index), "finalMask", "image", "imagine delimitata", "imagineRegionata"]  # lista cu numele fiecarei imagini
+        numar_de_imagini_pe_linie = 3
         if not pause:
             afisare(imagini, texte, numar_de_imagini_pe_linie)
         else:
