fb5b5602e5cd34e6a9637af727d31b23dfc21172
Viorel <viorel.stoica@student.tuiasi.ro>
2023-12-30 19:15:11 +0200

    mai multe masti, culori diferite

--- GenerareMasca.py
+++ GenerareMasca.py
@@ -2,8 +2,12 @@ import numpy as np
 import cv2
 
 listaPuncte = list()
-numarMasti = 0
+listaRegiuniDelimitare = list()
+numarRegiuniSalvate = 0
+index_regiune_curenta = 0
 modAfisareAlternativ = False
+
+
 def calculPunctMijloc(lista_puncte):
     maximx = lista_puncte[0][0]
     minimx = maximx
@@ -25,13 +29,13 @@ def calculPunctMijloc(lista_puncte):
     return mijlocx, mijlocy
 
 
-def generareMascaDelimitare(lista_puncte, latime, inaltime):
+def generareMascaDelimitare(imagine_sursa, lista_puncte, latime, inaltime, numarRegiune):
     mascaCreata = np.zeros((int(latime)+2, int(inaltime)+2), np.uint8)
-    imagine_delimitata = np.zeros((int(latime), int(inaltime)), np.uint8)
-    culoare = (155, 155, 155)
+    # imagine_delimitata = np.zeros((int(latime), int(inaltime)), np.uint8)
+    culoare = (50 + numarRegiune * 10, 50 + numarRegiune * 10, 50 + numarRegiune * 10)
     if len(lista_puncte) >= 2:  # desenam liniile initiale
         for index_puncte in range(-1, len(lista_puncte) - 1, 1):
-            cv2.line(imagine_delimitata, lista_puncte[index_puncte], lista_puncte[index_puncte + 1], culoare, 1)
+            cv2.line(imagine_sursa, lista_puncte[index_puncte], lista_puncte[index_puncte + 1], culoare, 1)
         if len(lista_puncte) >= 3:  # facem fill
             punctMijloc = calculPunctMijloc(lista_puncte)
             # print(punctMijloc)
@@ -53,19 +57,19 @@ def generareMascaDelimitare(lista_puncte, latime, inaltime):
             # cv2.circle(imagine_delimitata, (int(x_maxim_stanga), int(punctMijloc[1])), 10, (255, 0, 0), 3)
             # cv2.circle(imagine_delimitata, (int(x_maxim_dreapta), int(punctMijloc[1])), 10, (255, 0, 0), 3)
             if x_maxim_stanga != punctMijloc[0]:
-                cv2.floodFill(imagine_delimitata, mascaCreata, (int(x_maxim_stanga)+5, int(punctMijloc[1])), culoare)
+                cv2.floodFill(imagine_sursa, mascaCreata, (int(x_maxim_stanga)+5, int(punctMijloc[1])), culoare)
             else:
-                cv2.floodFill(imagine_delimitata, mascaCreata, (int(x_maxim_dreapta)-5, int(punctMijloc[1])), culoare)
+                cv2.floodFill(imagine_sursa, mascaCreata, (int(x_maxim_dreapta)-5, int(punctMijloc[1])), culoare)
     # else:
         # print("Nu am destule puncte!" + str(len(lista_puncte)))
-    return imagine_delimitata
+    return imagine_sursa
 
 
-def regiuniDelimitare(imagine, lista_puncte):
+def regiuniDelimitare(imagine, lista_puncte, culoare):
     imag_aux = imagine.copy()
     if len(lista_puncte) >= 2:  # desenam liniile initiale
         for index_puncte in range(-1, len(lista_puncte) - 1, 1):
-            cv2.line(imag_aux, lista_puncte[index_puncte], lista_puncte[index_puncte + 1], (255, 0, 0), 3)
+            cv2.line(imag_aux, lista_puncte[index_puncte], lista_puncte[index_puncte + 1], culoare, 3)
     return imag_aux
 
 
@@ -97,7 +101,8 @@ def callbackButonAfisareAlternativa(par1, par2):
 if __name__ == '__main__':
     current_frame_index = 0
     nume_imagine_filtru = 'Editare masca'
-    cv2.namedWindow(nume_imagine_filtru)
+    cv2.namedWindow(nume_imagine_filtru, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(nume_imagine_filtru,cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.setMouseCallback(nume_imagine_filtru, callbackMouse)
     cv2.createButton("Incarcare masca", callbackButonIncarcareMasca, cv2.QT_PUSH_BUTTON)
     cv2.createButton("Salvare masca", callbackButonSalvareMasca, cv2.QT_PUSH_BUTTON)
@@ -115,13 +120,29 @@ if __name__ == '__main__':
         key = cv2.waitKey(16)
         if key == ord('q'):  # Press 'q' to exit
             break
+        if key == ord('w'): # w sa trec la urmatoarea regiune
+            if len(listaPuncte) >= 3:
+                listaRegiuniDelimitare.append(list(listaPuncte))
+                numarRegiuniSalvate += 1
+                index_regiune_curenta = numarRegiuniSalvate
+                listaPuncte.clear()
+            else:
+                print("nu sunt destule puncte pentru a salva regiunea!")
+
         # aplicare filtru delimitare pe poza originala
-        MascaDelimitare = generareMascaDelimitare(listaPuncte, latime, inaltime)
-        imagineRegionata = regiuniDelimitare(frame, listaPuncte)
-        cv2.putText(imagineRegionata, "Regiunea selectata: ", (5, 25), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (250, 50, 250), 1, cv2.LINE_AA)
-        cv2.putText(imagineRegionata, "Numar regiuni: ", (5, 50), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (250, 50, 250), 1, cv2.LINE_AA)
+        MascaDelimitare = np.zeros((int(latime), int(inaltime)), np.uint8)
+        imagineRegionata = frame.copy()
+        for puncteRegiune in listaRegiuniDelimitare:
+            imagineRegionata = regiuniDelimitare(imagineRegionata, puncteRegiune, (0, 255, 0))  # regiunile salvate deja
+            MascaDelimitare = generareMascaDelimitare(MascaDelimitare, puncteRegiune, latime, inaltime,
+                                                      listaRegiuniDelimitare.index(puncteRegiune))
+        MascaDelimitare = generareMascaDelimitare(MascaDelimitare, listaPuncte, latime, inaltime, numarRegiuniSalvate)
+        imagineRegionata = regiuniDelimitare(imagineRegionata, listaPuncte, (0, 0, 255))  # regiunea inca in lucru
+
+        cv2.putText(imagineRegionata, "Regiunea selectata: "+str(index_regiune_curenta), (5, 25), cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (50, 250, 50), 2, cv2.LINE_AA)
+        cv2.putText(imagineRegionata, "Numar regiuni salvate: " + str(numarRegiuniSalvate), (5, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (50, 250, 50), 2, cv2.LINE_AA)
 
         if modAfisareAlternativ is True:
             cv2.imshow(nume_imagine_filtru, MascaDelimitare)
