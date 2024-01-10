0daeccad931a58bc624d100c5c86799973cb2865
Viorel <viorel.stoica@student.tuiasi.ro>
2024-01-11 00:09:05 +0200

    reparat bug la pauza in plotari

--- Plotari.py
+++ Plotari.py
@@ -5,12 +5,12 @@ import cv2
 def generarePlot(valori, latime, inaltime, culoare=(255, 0, 0)):
     imagine_sursa = np.zeros((int(inaltime), int(latime), 3), np.uint8)
     max_val = max(valori)
-    for index in range(0, len(valori)):
-        y_sus = inaltime - int(inaltime/max_val)*valori[index]
-        x_stanga = int(latime/len(valori))*index
-        x_dreapta = x_stanga+int(latime/len(valori))
-        cv2.rectangle(imagine_sursa, (x_stanga+2, y_sus), (x_dreapta-2, inaltime), culoare, -1, cv2.LINE_8)
-        cv2.
+    if max_val != 0:
+        for index in range(0, len(valori)):
+            y_sus = inaltime - int(inaltime/max_val)*valori[index]
+            x_stanga = int(latime/len(valori))*index
+            x_dreapta = x_stanga+int(latime/len(valori))
+            cv2.rectangle(imagine_sursa, (x_stanga+2, y_sus), (x_dreapta-2, inaltime), culoare, -1, cv2.LINE_8)
     return imagine_sursa
 
 
